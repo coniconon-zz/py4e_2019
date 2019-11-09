e1554e863d64de1a6183f7fa86db0ddcf262d9fa
Constanza Morales <coniconon@users.noreply.github.com>
2019-11-09 15:14:28 -0300

    Finished code for assignment 9.4

--- course_2/assignment_09_04/assingment_09_04.py
+++ course_2/assignment_09_04/assingment_09_04.py
@@ -1,3 +1,20 @@
-name = input("Enter file:")
-if len(name) < 1 : name = "mbox-short.txt"
-handle = open(name)
+filename = input("Enter file: ")
+if len(filename) < 1 : filename = "mbox-short.txt"
+handle = open(filename)
+
+# Create a dictionary
+email = dict()
+
+# Loop to read each line in the text and split them
+for line in handle:
+    if line.startswith('From '):       
+        line = line.split()
+        address = line[1]
+        email[address] = email.get(address, 0) + 1
+
+most = None
+for key in email:
+    if most is None or email[key] > most:
+        most = email[key]
+        sender = key
+print(sender, most)
